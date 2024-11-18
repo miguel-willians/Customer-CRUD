7c1beb09d81c0cee6d5432aaea885f94582937fd
miguel-willians <miiguel.wso@gmail.com>
2024-11-18 15:22:41 -0300

    Configuring remote database

--- src/main/java/com/crud/cadastro/CadastroApplication.java
+++ src/main/java/com/crud/cadastro/CadastroApplication.java
@@ -1,13 +1,18 @@
 package com.crud.cadastro;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CadastroApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CadastroApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // Carregar variáveis do arquivo .env
+        Dotenv dotenv = Dotenv.configure().load();
+        System.setProperty("PGPASSWORD", dotenv.get("PGPASSWORD"));
 
+        // Iniciar a aplicação Spring Boot
+        SpringApplication.run(CadastroApplication.class, args);
+    }
 }
