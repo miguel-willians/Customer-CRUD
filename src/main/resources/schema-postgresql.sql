a5e4517df2b6824788f2dcdff12aead413f2455a
miguel-willians <miiguel.wso@gmail.com>
2024-11-18 16:46:14 -0300

    Debugging db

--- src/main/resources/schema-postgresql.sql
+++ src/main/resources/schema-postgresql.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS customer CASCADE;
 
 CREATE TABLE IF NOT EXISTS customer (
     cd_customer SERIAL PRIMARY KEY,
-    nm_customer VARCHAR(20) NOT NULL UNIQUE,
+    nm_customer VARCHAR(20) NOT NULL,
     ds_email VARCHAR(35) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
